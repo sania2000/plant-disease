39a6944cd8cc8ab2cf348f1df47faad9c33f3257
Sania Safari <saniasafariii@gmail.com>
2022-04-07 12:14:50 +0430

    final

--- server.js
+++ server.js
@@ -22,23 +22,23 @@ const upload = multer({ storage: storage });
 let responses = []
 
 app.post("/disease", upload.single("image"), async (req, res) => {
-    let form = new formData();
-    form.append("organs", "leaf");
-    form.append("images", fs.createReadStream("./images/100.jpg"));
+    // let form = new formData();
+    // form.append("organs", "leaf");
+    // form.append("images", fs.createReadStream("./images/100.jpg"));
 
-    try {
-        const { status, data } = await axios.post(API_URL, form, {
-            headers: form.getHeaders(),
-        });
-        setTimeout(async function () {
-            console.log("status", status);
-            for (i = 0; i < 1; i++) {
-                responses.push(data.results[i]);
-            }
-        }, 3);
-    } catch (error) {
-        console.error("error", error);
-    }
+    // try {
+    //     const { status, data } = await axios.post(API_URL, form, {
+    //         headers: form.getHeaders(),
+    //     });
+    //     setTimeout(async function () {
+    //         console.log("status", status);
+    //         for (i = 0; i < 1; i++) {
+    //             responses.push(data.results[i]);
+    //         }
+    //     }, 3);
+    // } catch (error) {
+    //     console.error("error", error);
+    // }
 
     const files = ["./images/100.jpg"];
 
@@ -68,10 +68,12 @@ app.post("/disease", upload.single("image"), async (req, res) => {
                             return el.probability >= 0.1;
                         }
                     );
+                    responses.push(ress.data.suggestions[0].plant_name)
                     responses.push('health probabilty:' + ress.data.health_assessment.is_healthy_probability);
                     responses.push(health_details);
                 }
                 else {
+                    responses.push(ress.data.suggestions[0].plant_name)
                     responses.push("Your plant is healthy")
                 }
                 res.send(responses)
