bb516fd1f7d5a12a15095e3e8a148229744adb55
Sania Safari <saniasafariii@gmail.com>
2022-05-08 00:41:02 +0430

    f

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 const { status } = require("express/lib/response");
     
 
-let id = 4;
+let id = 10;
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "images");
